31de44a8f5168ce7eb1d5db8b55b49d1e0680de8
Jenny Yang <jiehning71017@gmail.com>
2025-05-23 16:43:13 +0800

    update combine bitmap version of API

--- ssd1306_rpi5/ssd1306_API/ssd1306_API.c
+++ ssd1306_rpi5/ssd1306_API/ssd1306_API.c
@@ -16,6 +16,12 @@ static char msg[84]={0}; //(128/6)*4
 int main(void){
 	int file_desc;
 	
+	int ret = system("/home/pi/snd2txt/snd2txt.sh");
+	if (ret == -1){
+		perror("system");
+		return 1;
+	}	
+	
 	if ((file_desc = open(dev, O_RDWR)) <0){
 		printf("Cannot open device file:%s\n", dev);
 		exit(-1);
@@ -25,11 +31,13 @@ int main(void){
 	printf("fd = %d\n");
 	printf("getpid = %d\n", getpid());
 	*/
-	FILE *file = fopen("test.txt", "r");
+	
+	FILE *file = fopen("/home/pi/snd2txt/result.txt", "r");
 	if (file == NULL){
 		perror("fail to open result.txt");
 		return 1;
 	}
+	
 
 	int contrast = 0xff;
 	int i = 0;
@@ -37,11 +45,16 @@ int main(void){
 		if (write(file_desc, msg, sizeof(msg))<0){
 			printf("write failed\n");
 		}
-		contrast >>= i++;
+		
+		//adjust contrast
+		contrast >>= i++; 
 		ioctl(file_desc, OLED_CONTRAST,&contrast );
+		
 		sleep(4);
 	}
 	ioctl(file_desc, OLED_CLEAR);
+	
+	//draw bitmap using mmap()
 	uint8_t *fb = mmap(NULL, 513, PROT_READ|PROT_WRITE, MAP_SHARED,file_desc,0);
 	if (fb == MAP_FAILED){
 		perror("mmap failed");
@@ -49,12 +62,14 @@ int main(void){
 	}
 	ioctl(file_desc, OLED_CLEAR_FB);
 	memcpy(fb+1, oasis_bitmap, 512);
+
 	if (ioctl(file_desc, OLED_MMAP_DRAW)<0){
 		perror("ioctl failed");
 	}
 	sleep(3);
 	ioctl(file_desc, OLED_CLEAR_FB);
 	ioctl(file_desc, OLED_MMAP_DRAW);
+	munmap(fb, 513);
 	fclose(file);
 	close(file_desc);
 	return 0;
